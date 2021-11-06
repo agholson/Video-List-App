da71c2ea74d5afcfe14d89017122bfab760b00fa
agholson <87455899+agholson@users.noreply.github.com>
2021-11-06 12:20:50 -0400

    Initial Commit

---
+++ AllVideosModule5WrapUp/AllVideosModule5WrapUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllVideosModule5WrapUpApp.swift
+//  AllVideosModule5WrapUp
+//
+//  Created by Shepherd on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct AllVideosModule5WrapUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllVideosModule5WrapUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AllVideosModule5WrapUp
+//
+//  Created by Shepherd on 11/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
